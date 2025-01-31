21a9617e0d920392db2c4fef42a67f89687f4540
ashaa <ashaa@LAPTOP-CAFB9C5N>
2025-02-01 05:28:00 +0530

    Add project files.

---
+++ SocialApplication/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialApplication.Entities;
+
+namespace SocialApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController: ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsrs()
+        {
+            var users = await _context.Users.ToListAsync();
+            if(users==null)
+            {
+                return NotFound();
+            }
+            return Ok(users);
+
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if(user!=null)
+            {
+                return Ok(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddUser(AppUser user)
+        {
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok();    
+
+        }
+
+    }
+}
---
+++ SocialApplication/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialApplication.Entities;
+
+
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+
+}
+
