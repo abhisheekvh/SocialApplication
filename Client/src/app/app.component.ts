b27b07e1c60ec98178d26fe77ba17cc146d4a7fb
Abhishek <hayagreevvishnu@gmail.com>
2025-05-13 20:44:02 +0530

    Added client side project

---
+++ Client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { error } from 'console';
+import { response } from 'express';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  
+  http=inject(HttpClient)
+  title = 'SocialApp by abhi';
+  users:any;
+  ngOnInit(): void {
+    this.http.get("https://localhost:7061/api/Users").subscribe({
+      next: response=>this.users=response,
+      error:error=>console.log(error),
+      complete:()=>console.log("Request has completed")
+    })
+   
+  }
+}
